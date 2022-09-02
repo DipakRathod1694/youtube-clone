d3bac1306fad65b2bf50820e737a118e0c820b64
Dipak Rathod <rathoddipak1694@gmail.com>
2022-09-03 01:10:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import youtube from "./api/youtube";
+import { Grid } from "@material-ui/core";
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid style={{justifyContent: "center"}} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit = {handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            {/* {VideoDetail} */}
+          </Grid>
+          <Grid item xs={4}>
+            {/* {VideoList} */}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
+
+  async function handleSubmit(searchItem){
+    const response = await youtube.get("serach",{
+      part: "snippet",
+      maxResults: 5,
+      key: "AIzaSyC3v8BXGYQAIPE4X1tMm-vrZaneLDucaMQ",
+      q: searchItem
+    });
+    console.log(response);
+  }
 }
 
 export default App;
